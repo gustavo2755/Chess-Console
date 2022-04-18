a82b76fbcb4ed04ae87f2d49fe01ca31a10820e8
Gustavo Oliveira <gu.ferreira2@hotmail.com>
2022-04-18 18:22:14 -0300

    1 class

---
+++ Xadrez-console/Table/Possition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Xadrez_console.Table
+{
+    internal class Possition
+    {
+        public int Line { get; set; }
+        public int Colun { get; set; }
+
+        public Possition(int line, int colun)
+        {
+            Line = line;
+            Colun = colun;
+        }
+
+        public override string ToString()
+        {
+            return " line :" + Line + " colun :" + Colun; 
+        }
+    }
+}
